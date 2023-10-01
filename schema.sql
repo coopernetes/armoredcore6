30f41dddf6903506928f094783b37999caac1519
Thomas Cooper <57812123+coopernetes@users.noreply.github.com>
2023-10-01 11:18:48 -0400

    add remaining tables

--- schema.sql
+++ schema.sql
@@ -77,3 +77,59 @@ parts_frame_legs (
     description TEXT,
     image BLOB
 );
+
+CREATE TABLE IF NOT EXISTS
+parts_internal_booster (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    manufacturer TEXT,
+    thrust INTEGER,
+    upward_thrust INTEGER,
+    upward_en_consumption INTEGER,
+    qb_thrust INTEGER,
+    qb_jet_duration REAL,
+    qb_en_consumption INTEGER,
+    qb_reload_time REAL,
+    qb_reload_ideal_weight INTEGER,
+    ab_thrust INTEGER,
+    ab_en_consumption INTEGER,
+    melee_atk_thrust INTEGER,
+    melee_atk_en_consumption INTEGER,
+    weight INTEGER,
+    en_load INTEGER,
+    description TEXT,
+    image BLOB
+);
+
+CREATE TABLE IF NOT EXISTS
+parts_internal_fcs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    manufacturer TEXT,
+    close_assist INTEGER,
+    medium_assist INTEGER,
+    long_assist INTEGER,
+    avg_assist INTEGER,
+    missile_correction INTEGER,
+    multi_lock_correction INTEGER,
+    weight INTEGER,
+    en_load INTEGER,
+    description TEXT,
+    image BLOB
+);
+
+CREATE TABLE IF NOT EXISTS
+parts_internal_generator (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    manufacturer TEXT,
+    en_capacity INTEGER,
+    en_recharge INTEGER,
+    supply_recovery INTEGER,
+    post_recovery_en_supply INTEGER,
+    energy_firearm_spec INTEGER,
+    weight INTEGER,
+    en_load INTEGER,
+    description TEXT,
+    image BLOB
+);

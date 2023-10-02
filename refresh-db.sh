152bf6db5925ca34433c662e6f1dd907e21fb4ae
Thomas Cooper <57812123+coopernetes@users.noreply.github.com>
2023-10-02 00:35:55 -0400

    db update

--- refresh-db.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-rm -f ./data/ArmoredCore6.db
-sqlite3 ./data/ArmoredCore6.db < schema.sql
-sqlite3 ./data/ArmoredCore6.db < test.sql
-(cd WikidotScraper && dotnet clean)
-
